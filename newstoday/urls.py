e4382d61d843584f1371c0ccaff6aa96c40bbfe9
dongooree <pde0406@gmail.com>
2022-07-18 16:38:42 +0900

    heroku test

---
+++ newstoday/urls.py
@@ -0,0 +1,25 @@
+"""newstoday URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import * # 루트 디렉토리의 views에서 모든 함수를 가져온다.
+# djangoMaster>views.py에서 모든 함수를 가져온다.
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', main), # views.py의 main함수를 의미한다.
+]
+
